d0700c3131fbf094a3407bf3b5c766d486bb920a
Viktoria <android1986mir@rambler.ru>
2023-02-06 15:14:52 +0300

    Answer 3

---
+++ answer3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+//void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("этот день выходной");
+  }else if (dayNumber >= 1 && dayNumber <= 5 ) {
+    Console.WriteLine("этот день не выходной");
+  }else{
+    Console.WriteLine("Вы ввели не верное значение");
+  }
+
+  
