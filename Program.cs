a388b5b0c6e5c9984da0c79fda447652f810008d
Viktoria <android1986mir@rambler.ru>
2023-02-06 12:53:01 +0300

    Answer 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int result = num / 10 -((num / 100) * 10);
+Console.WriteLine($"{num} -> {result}");
+
